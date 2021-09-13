b038844d4b4e576fd45f2972e7d179be54cbf94d
Vivek <vivekpanchal1999@gmail.com>
2021-09-13 13:10:57 +0530

    .

--- src/App.js
+++ src/App.js
@@ -3,33 +3,29 @@ import './App.css';
 // import About from './Component/About';
 import Navbar from './Component/Navbar';
 import React, { useState } from 'react';
-import TextForm from './Component/TextForm'
+import TextForm from './Component/TextForm';
 
 function App() {
   const [mode, setMode] = useState("light");
-
   const toggleMode = () => {
     if (mode === 'light') {
-      setMode ('dark');
+      setMode('dark');
       document.body.style.backgroundColor = '#010621';
     }
     else {
-      setMode ('light');
+      setMode('light');
       document.body.style.backgroundColor = 'white';
     }
   }
+  return (
+    <>
+     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze" mode={mode} />
+        {/* <About/> */}
+      </div>
+    </>
+  );
+}
 
-    return (
-      <>
-
-
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-        <div className="container my-3">
-          <TextForm heading="Enter the text to analyze" mode={mode}  />
-          {/* <About/> */}
-        </div>
-      </>
-    );
-  }
-
-  export default App;
+export default App;
